ad17bec02dcdeaf3863f6ccd7b7157993d149ace
Van5sh <164542794+Van5sh@users.noreply.github.com>
2024-04-25 19:16:28 +0530

    Update groundf.cpp

--- groundf.cpp
+++ groundf.cpp
@@ -502,7 +502,190 @@ class watch
         }
 };
 
-class buying:public max,public watch
+string ask20="Yes";
+class Glasses
+{
+    public:
+        void directions(){
+        string table[5][3]=
+            {
+            {"Eyeglasses","\t\t","Left <--"},
+            {"Sunglasses","\t","Right -->"},
+            {"Contact_lenses","\t\t\t","Forward and Right ↱"},
+            {"Billing","\t\t\t","Forward and Left ↰"},
+            };
+            for(int i=0;i<5;i++)
+            {
+                for(int j=0;j<3;j++)
+                {
+                    cout<<table[i][j];
+                }
+                cout<<"\t"<<endl;
+            }
+            }
+        void menu()
+        {
+            cout<<"             We exist to transform the way people see and experience the world"<<endl;
+            string table1[10][5]=
+            {
+            {"SL.No","\t","  ITEMS","\t\t\t","   RANGE"},
+            {" 1","\t","Sunglasses","\t\t","Rs 2000-5000 "},
+            {" 2","\t","Eyeglasses","\t\t","Rs 4000-12000"},
+            {" 3","\t","Contact_lenses","\t\t","Rs 6000-13000"},
+            };
+            for(int i=0;i<9;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    cout<<table1[i][j];
+                }
+                cout<<"\t"<<endl;
+            }
+        }
+};
+
+class talk
+{
+    public:
+        string ask1,a,ask,ask3;
+        int sum=0,n,n1;
+        void price(string a)
+        {
+            if(ask=="A1")
+                sum+=1500;
+            else if(ask=="A2")
+                sum+=2700;
+            else if(ask=="A3")
+                sum+=4300;
+            else if(ask=="W1")
+                sum+=1500;
+            else if(ask=="W2")
+                sum+=2200;
+            else if(ask=="W3")
+                sum+=5000;
+            else if(ask=="S1")
+                sum+=1200;
+            else if(ask=="S2")
+                sum+=2000;
+            else if(ask=="S3")
+                sum+=4000;
+            else if(ask=="PNL")
+                sum+=4200;
+            else if(ask=="MNL")
+                sum+=2700;
+            else if(ask=="RNL")
+                sum+=4700;
+            else if(ask=="PBL")
+                sum+=5200;
+            else if(ask=="MBL")
+                sum+=4500;
+            else if(ask=="RBL")
+                sum+=9200;
+            else if(ask=="SFC")
+                sum+=5200;
+            else if(ask=="RCL")
+                sum+=4700;
+            else if(ask=="EWL")
+                sum+=5000;
+        }
+        void frame1(){
+        string table[7][5]={
+            {"SR.No","\t","FRAMES AND LENSEs","\t\t","    RANGE"},
+            {" 1","\t","Plastic and Normal Lenses(PNL)","\t"," Rs. 4200"},
+            {" 2","\t","Metal and Normal Lenses(MNL)","\t"," Rs. 2700"},
+            {" 3","\t","Rimless and Normal Lenses(RNL)","\t"," Rs. 4700"},
+            {" 4","\t","Plastic and Blue Lenses(PBL)","\t"," Rs. 5200"},
+            {" 5","\t","Metal and Blue Lenses(MBL)","\t"," Rs. 4500"},
+            {" 6","\t","Rimless and Blue Lenses(RBL)","\t"," Rs. 9200"}};
+            for(int i=0;i<7;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    cout<<table[i][j];
+                }
+                cout<<"\t"<<endl;
+            }
+        }
+        void clenses(){
+            string table[4][5]={
+            {"SR.No","\t","Contact Lenses","\t","    RANGE"},
+            {" 1","\t","Soft Contact lenses(SFC)","\t"," Rs. 5200"},
+            {" 2","\t","Rigid Contact lenses(RCL)","\t"," Rs. 4700"},
+            {" 3","\t","Extended wear Contact lenses(EWL)","\t"," Rs. 5000"}};
+            for(int i=0;i<4;i++)
+            {
+                for(int j=0;j<5;j++)
+                {
+                    cout<<table[i][j];
+                }
+                cout<<"\t"<<endl;
+            }
+        }
+        void sale()
+        {
+            cout<<"Which section do you want to go ";cin>>ask1;
+            if (ask1=="Sunglasses")
+            {
+                cout<<"Which brand do you prefer"<<endl;
+                cout<<"Aviators\tWayfarers\tSports"<<endl;
+                cout<<"I would like to see";cin>>ask3;
+                if (ask3=="Aviators")
+                {
+                    cout<<"A Model1(A1)\t"<<"1500"<<endl;
+                    cout<<"A Model2(A2)\t"<<"2700"<<endl;
+                    cout<<"A Model3(A3)\t"<<"4300"<<endl;
+                    cout<<"i would like to buy ";cin>>ask;
+                    cout<<"Let's go to another section"<<endl;
+                    }
+                else if(ask3=="Wayfarers")
+                    {
+                    cout<<"W Model1(W1)\t"<<"1500"<<endl;
+                    cout<<"W Model2(W2)\t"<<"2200"<<endl;
+                    cout<<"W Model3(W3)\t"<<"5000"<<endl;
+                    cout<<"I would like to buy ";cin>>ask;
+                    cout<<"Let's go to another section"<<endl;
+                    }
+                 else if(ask3=="Sports")
+                    {
+                    cout<<"S Model1(S1)\t"<<"1200"<<endl;
+                    cout<<"S Model2(S2)\t"<<"2000"<<endl;
+                    cout<<"S Model3(S3)\t"<<"4000"<<endl;
+                    cout<<"I would like to buy ";cin>>ask;
+                    cout<<"Let's go to another section"<<endl;
+                    }
+            }
+            string  ask4,ask5;
+            if(ask1=="Eyeglasses")
+            {
+               cout<<"What kind of frames do you have"<<endl;
+               frame1();
+               cout<<"I would like to choose";cin>>ask;
+               cout<<"Let's go to another section"<<endl;
+            }
+            else if(ask1=="Contact_lenses")
+            {
+                cout<<"Hello sir! Which kind of contact lenses you prefer"<<endl;
+                clenses();
+                cout<<"I would like to buy";cin>>ask;
+                cout<<"Let's go to another section"<<endl;
+            }
+            price(ask);
+        }
+        void bill()
+        {
+            cout<<"Your total bill is:"<<sum<<endl;
+                time_t currentTime = time(nullptr);
+                tm* localTime = localtime(&currentTime);
+                cout
+                  << localTime->tm_hour << ":"
+                  << localTime->tm_min << ":"
+                  << localTime->tm_sec <<endl;
+            amount=amount-sum;
+            cout<<"Amount left with you:"<<amount<<endl;
+        }
+
+};
+class buying:public max,public watch,public talk
 {
     public:
     void layout()
@@ -540,6 +723,19 @@ class buying:public max,public watch
             g.ask();
         }
         g.bill();
+    }
+    void eyes()
+    {
+        Glasses gl;
+        talk t;
+        gl.menu();
+        cout<<"---------Welcome to Lenskart---------"<<endl;
+        t.sale();
+        while(ask20=="Yes"){
+            t.sale();
+            }
+        t.bill();
+
     }
 };
 
@@ -638,6 +834,12 @@ int main()
         else if(aski=="WOW")
         {
             b.watch1();
+            cout<<"Do you want to buy more on this floor"<<endl;
+            cin>>ask7;
+        }
+        else if(aski=="Lenskart")
+        {
+            b.eyes();
             cout<<"Do you want to buy more on this floor"<<endl;
             cin>>ask7;
         }
